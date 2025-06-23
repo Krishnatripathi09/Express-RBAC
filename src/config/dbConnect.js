862a06c325aff937394715e152991362613a2cd3
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-06-23 16:08:30 +0530

    chore: add .env to .gitignore to prevent pushing sensitive environment variables

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -2,10 +2,15 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 
 const connectDB = async () => {
-  const connect = await mongoose.connect(process.env.CONNECTION_STRING);
-  console.log(
-    `DataBase Connection SuccessFull : ${connect.connection.host},${connect.connection.name}`
-  );
+  try {
+    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+    console.log(
+      `DataBase Connection SuccessFull : ${connect.connection.host},${connect.connection.name}`
+    );
+  } catch (err) {
+    console.log("Error Occured :", err);
+    process.exit(1);
+  }
 };
 
 module.exports = {
